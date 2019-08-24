722de55a944e714ac5778d2df686ba7cf2393c79
Joe Cooper <josephcooper3@gmail.com>
2019-08-24 18:08:06 +0100

    add and test title and description variables of Product class

--- models/product.rb
+++ models/product.rb
@@ -1,11 +1,12 @@
 class Product
 
   attr_reader :id
-  attr_accessor :title
+  attr_accessor :title, :description
 
   def initialize(options)
     @id = options['id'] if options['id']
     @title = options['title']
+    @description = options['description']
   end
 
 end
--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -7,12 +7,22 @@ class ProductTest < MiniTest::Test
   def setup
     @product1 = Product.new({
       'id' => 1,
-      'title' => 'Water Lilies'
+      'title' => 'Water Lilies',
+      'description' => 'Print on gloss paper with card mount, 20cm x 35cm'
       })
   end
 
   def test_product_has_id
     assert_equal(1, @product1.id())
+  end
+
+  def test_product_has_title
+    assert_equal('Water Lilies', @product1.title())
+  end
+
+  def test_product_has_description
+    description = 'Print on gloss paper with card mount, 20cm x 35cm'
+    assert_equal(description, @product1.description())
   end
 
 end
