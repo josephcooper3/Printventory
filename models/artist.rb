623ce19e43072114d6ff341c8711865686a71476
Joe Cooper <josephcooper3@gmail.com>
2019-08-25 19:03:46 +0100

    adds Artist.find_by_id method

--- models/artist.rb
+++ models/artist.rb
@@ -53,4 +53,12 @@ class Artist
     SqlRunner.run(sql)
   end
 
+  def self.find_by_id(id)
+    sql = "SELECT * FROM artists
+    WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)[0]
+    return Artist.new(result)
+  end
+
 end
