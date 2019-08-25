7c1a33498925bf5bbb3b955a6ec8a1f153ab54df
Joe Cooper <josephcooper3@gmail.com>
2019-08-25 18:48:43 +0100

    specify artist id as integer

--- models/artist.rb
+++ models/artist.rb
@@ -6,7 +6,7 @@ class Artist
   attr_accessor :first_name, :last_name
 
   def initialize(options)
-    @id = options['id'] if options['id']
+    @id = options['id'].to_i if options['id']
     @first_name = options['first_name']
     @last_name = options['last_name']
   end
