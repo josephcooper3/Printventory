812247035c6a9f697805d7277b96e8fc00458007
Joe Cooper <josephcooper3@gmail.com>
2019-08-25 13:23:13 +0100

    add and test copies_in_stock variable of Product class

--- models/product.rb
+++ models/product.rb
@@ -1,7 +1,7 @@
 class Product
 
   attr_reader :id
-  attr_accessor :title, :description, :artist_id, :buying_cost, :sale_price
+  attr_accessor :title, :description, :artist_id, :buying_cost, :sale_price, :copies_in_stock
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
@@ -10,6 +10,7 @@ class Product
     @artist_id = options['artist_id'].to_i
     @buying_cost = options['buying_cost'].to_f
     @sale_price = options['sale_price'].to_f
+    @copies_in_stock = options['copies_in_stock'].to_i
   end
 
 end
--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -11,7 +11,8 @@ class ProductTest < MiniTest::Test
       'description' => 'Print on gloss paper with card mount, 20cm x 35cm',
       'artist_id' => 2,
       'buying_cost' => 5.59,
-      'sale_price' => 12.99
+      'sale_price' => 12.99,
+      'copies_in_stock' => 5
       })
   end
 
@@ -38,6 +39,10 @@ class ProductTest < MiniTest::Test
 
   def test_product_has_sale_price
     assert_equal(12.99, @product1.sale_price())
+  end
+
+  def test_product_has_copies_in_stock
+    assert_equal(5, @product1.copies_in_stock())
   end
 
 end
