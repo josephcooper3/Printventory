5ec280e9c3d411059e83ebc2186366c2e9631645
Joe Cooper <josephcooper3@gmail.com>
2019-08-26 14:06:54 +0100

    adds title to layout and text to index

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('controllers/artist_controller.rb')
-require_relative('controllers/product_controller.rb')
+require_relative('controllers/artists_controller.rb')
+require_relative('controllers/products_controller.rb')
 also_reload('./models/*')
 
 get '/' do
